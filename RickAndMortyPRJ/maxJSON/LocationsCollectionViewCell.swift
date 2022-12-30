05dc10b50f0054ea01f0def50bb83f32c957e4e8
Alviss0 <91151004+Alviss0@users.noreply.github.com>
2022-12-30 09:45:47 +0300

    Add files via upload

---
+++ RickAndMortyPRJ/maxJSON/LocationsCollectionViewCell.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class LocationsCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var dimensionLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+}
--- RickAndMortyPRJ/maxJSON/LocationsViewController.swift
+++ RickAndMortyPRJ/maxJSON/LocationsViewController.swift
@@ -1,30 +1,107 @@
-//
-//  LocationsViewController.swift
-//  RickAndMorty
-//
-//  Created by Алина Николенко on 30.12.2022.
-//  Copyright © 2022 MacStud. All rights reserved.
-//
-
 import UIKit
 
-class LocationsViewController: UIViewController {
+/*extension UIImageView {
+   func downloaded(from url: URL, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() { [weak self] in
+                self?.image = image
+            }
+            }.resume()
+    }
+    func downloaded(from link: String, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloaded(from: url, contentMode: mode)
+    }
+}
+*/
+    
+struct ParentLocation: Decodable{
+    let results: [Location]
+}
 
+struct Location: Decodable {
+    let name: String
+    let dimension: String
+}
+
+
+class LocationsViewController: UIViewController, UICollectionViewDataSource {
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var locations = [Location]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        collectionView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        collectionView.dataSource = self
+        
+        var urlString = "https://rickandmortyapi.com/api/location"
+        
+        print(urlString)
+        
+        guard let url = URL(string: urlString) else {return}
+        
+        URLSession.shared.dataTask(with: url) {data, response, error in
+            if let error = error{
+                print(error)
+                return
+            }
+            
+            guard let data = data else {return}
+            
+            
+            do{
+                let Location = try JSONDecoder().decode(ParentLocation.self, from:data)
+                self.locations = Location.results
+                
+            }catch{
+                print(error)
+            }
+            
+            DispatchQueue.main.async{
+                self.collectionView.reloadData()
+            }
+            
+            }.resume()
+        
     }
-    */
 
+func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    
+    return locations.count
+    
+}
+
+func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "locationsCell", for: indexPath) as! LocationsCollectionViewCell
+    
+    cell.dimensionLabel.text = locations[indexPath.row].dimension.capitalized
+    cell.nameLabel.text = locations[indexPath.row].name.capitalized
+
+    cell.layer.cornerRadius = 15
+
+    cell.clipsToBounds = true
+
+
+    return cell
+
+
+}
+    
 }
